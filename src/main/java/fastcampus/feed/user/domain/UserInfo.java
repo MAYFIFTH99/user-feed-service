03f9d320311da58c4b84c79488d30ded3959bc0f
harvey <alstjr971@naver.com>
2025-01-22 22:44:25 +0900

    feat: User 도메인 설계 -> 생성자 이름 검증과 팔로워 기능 추가, VO 이용

---
+++ src/main/java/fastcampus/feed/user/domain/UserInfo.java
@@ -0,0 +1,22 @@
+package fastcampus.feed.user.domain;
+
+public class UserInfo {
+    private String name;
+    private String profileImageUrl;
+
+    public UserInfo(String name, String profileImageUrl) {
+        if(name == null){
+            throw new IllegalStateException();
+        }
+        this.name = name;
+        this.profileImageUrl = profileImageUrl;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getProfileImageUrl() {
+        return profileImageUrl;
+    }
+}
