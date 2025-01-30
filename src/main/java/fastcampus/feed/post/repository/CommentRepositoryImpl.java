1b8bdd3a09f8aa6dd3844695ada80589600e2e5b
harvey <alstjr971@naver.com>
2025-01-31 02:43:28 +0900

    feat: 댓글 생성 시 Post의 댓글 개수 추가 기능

--- src/main/java/fastcampus/feed/post/repository/CommentRepositoryImpl.java
+++ src/main/java/fastcampus/feed/post/repository/CommentRepositoryImpl.java
@@ -4,6 +4,7 @@ import fastcampus.feed.post.domain.comment.Comment;
 import fastcampus.feed.post.repository.entity.comment.CommentEntity;
 import fastcampus.feed.post.repository.interfaces.CommentRepository;
 import fastcampus.feed.post.repository.jpa.JpaCommentRepository;
+import fastcampus.feed.post.repository.jpa.JpaPostRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Repository;
 
@@ -12,9 +13,11 @@ import org.springframework.stereotype.Repository;
 public class CommentRepositoryImpl implements CommentRepository {
 
     private final JpaCommentRepository jpaCommentRepository;
+    private final JpaPostRepository jpaPostRepository;
 
     @Override
     public Comment save(Comment comment) {
+        jpaPostRepository.findById(comment.getPost().getId()).orElseThrow(IllegalArgumentException::new).increaseCommentCount();
         CommentEntity commentEntity = new CommentEntity(comment);
         return jpaCommentRepository.save(commentEntity).toComment();
     }
