fda94324a63159beaf377e821c71e69701724a73
harvey <alstjr971@naver.com>
2025-02-01 18:36:36 +0900

    test: DB를 초기화하는 번거로운 로직 제거
    1. @Transactional 사용
    2. @DirtiesContent 사용
    3. DataJpaTest, AutoConfigureTestDataBase 사용

--- src/test/java/fastcampus/feed/acceptance/steps/utils/AcceptanceTestTemplate.java
+++ src/test/java/fastcampus/feed/acceptance/steps/utils/AcceptanceTestTemplate.java
@@ -4,23 +4,23 @@ import org.junit.jupiter.api.BeforeEach;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
-import org.springframework.test.annotation.Rollback;
+import org.springframework.test.annotation.DirtiesContext;
 import org.springframework.test.context.ActiveProfiles;
 
 @ActiveProfiles("test")
 @SpringBootTest(webEnvironment = WebEnvironment.DEFINED_PORT)
-@Rollback(value = false)
+@DirtiesContext(classMode = DirtiesContext.ClassMode.AFTER_EACH_TEST_METHOD)
 public class AcceptanceTestTemplate {
 
-    @Autowired
-    private DatabaseCleanUp cleanUp;
+//    @Autowired
+//    private DatabaseCleanUp cleanUp;
 
     @Autowired
     private DataLoader loader;
 
     @BeforeEach
     public void init(){
-        cleanUp.execute();
+//        cleanUp.execute();
         loader.loadData();
     }
 
