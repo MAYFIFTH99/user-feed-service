4ccc5796d912b7367dc3fff6e7656f7db94ea811
harvey <alstjr971@naver.com>
2025-02-02 20:41:44 +0900

    feat: EntityNotFound HandlerException 추가

--- src/main/java/fastcampus/feed/common/domain/exception/ErrorCode.java
+++ src/main/java/fastcampus/feed/common/domain/exception/ErrorCode.java
@@ -7,7 +7,8 @@ public enum ErrorCode {
 
     INVALID_INPUT_VALUE(400,"Invalid Input Value"),
     NOT_FOUND(404,"Not Found"),
-    INTERNAL_SERVER_ERROR(500,"Server Error");
+    INTERNAL_SERVER_ERROR(500,"Server Error"),
+    ENTITY_NOT_FOUND(400,"엔티티를 찾을 수 없습니다");
 
     private final Integer code;
     private final String message;
