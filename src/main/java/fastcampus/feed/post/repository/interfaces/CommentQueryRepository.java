42ba891811837b9fe62cf9c13213dff9e576fa53
harvey <alstjr971@naver.com>
2025-02-02 21:05:52 +0900

    docs: getCommentList JavaDocs 추가

--- src/main/java/fastcampus/feed/post/repository/interfaces/CommentQueryRepository.java
+++ src/main/java/fastcampus/feed/post/repository/interfaces/CommentQueryRepository.java
@@ -7,5 +7,13 @@ import org.springframework.stereotype.Repository;
 @Repository
 public interface CommentQueryRepository {
 
-    List<GetCommentContentResponseDto> getCommentList(Long postId,Long userId, Long lastContentId);
+    /**
+     +     * 사용자별 게시물의 댓글 목록을 조회합니다.
+     +     *
+     +     * @param userId 조회하는 사용자의 ID
+     +     * @param postId 댓글이 속한 게시물의 ID
+     +     * @param lastContentId 페이지네이션을 위한 마지막 컨텐츠 ID
+     +     * @return 댓글 목록
+     +     */
+    List<GetCommentContentResponseDto> getCommentList(Long postId, Long userId, Long lastContentId);
 }
